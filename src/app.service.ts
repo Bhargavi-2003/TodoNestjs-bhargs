dbee6638f7693b186a2b89dd6dd363fb35f27047
Bhargavi <gajjebhargavi14@gmail.com>
2024-07-15 23:50:51 +0530

    title update

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello! from Todo rest API!';
+    return 'Hey! You are at the right place. This is my Todo rest API!';
   }
 }
